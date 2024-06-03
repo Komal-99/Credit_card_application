7c2e107a29fe2b83a13125d75b5e086ab58343a1
Komal Gupta <komalgupta991000@gmail.com>
2024-06-03 16:18:45 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,8 +8,12 @@ PAGES = {
 
 def main_app():
     st.sidebar.title('Navigation')
-    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+    # Logout button in the sidebar
+    if st.sidebar.button('Logout'):
+        st.session_state['logged_in'] = False
+        st.session_state.pop('username', None)  # Optionally clear the username
+        st.experimental_rerun()  # Rerun the app to update the state
 
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
     page = PAGES[selection]
     page.app()
-
