0087205c87c5949c9ee974d5b0a8a5cafcc3b4a4
Komal Gupta <komalgupta991000@gmail.com>
2024-06-03 16:13:14 +0530

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -1,42 +1,34 @@
 import streamlit as st
 from authenticate import login, register
 from dashboard import main_app
+
 def main():
     st.title('Welcome to Credit Card Application')
-    st.sidebar.title("Navigation")
-    choice = st.sidebar.selectbox('Login/Signup', ['Login', 'Register'])
 
-    # Initialize logged_in status in the session state if not already present
     if 'logged_in' not in st.session_state:
         st.session_state['logged_in'] = False
+        st.session_state['navigation'] = 'Login'  # Default to login page
 
-    # Show Logout button if user is logged in
     if st.session_state['logged_in']:
-        if st.sidebar.button('Logout'):
-            st.session_state['logged_in'] = False
-            st.session_state['username'] = None  # Clear the username from session state
-            st.sidebar.success("You have been logged out.")
-            st.experimental_rerun()  # Rerun the app to refresh the state
-
-    # Handle login and registration choices
+        # Redirects to the main app/dashboard
+        main_app()
+    else:
+        choice = st.sidebar.selectbox('Login/Signup', ['Login', 'Register'])
+        handle_auth(choice)
+def handle_auth(choice):
     if choice == 'Login':
-        if st.session_state['logged_in']:
-            st.sidebar.success('Already logged in as {}'.format(st.session_state['username']))
-            main_app()  # Redirects to the main app/dashboard
-        else:
-            username = st.sidebar.text_input('Email')
-            password = st.sidebar.text_input('Password', type="password")
-            if st.sidebar.button('Login'):
-                if login(username, password):
-                    st.session_state['logged_in'] = True
-                    st.session_state['username'] = username
-                    st.sidebar.success('Logged in as {}'.format(username))
-                    main_app()  # Redirects to the main app/dashboard
-                else:
-                    st.sidebar.error('Incorrect username or password')
-
+        username = st.text_input('Email')
+        password = st.text_input('Password', type="password")
+        if st.button('Login'):
+            if login(username, password):
+                st.session_state['logged_in'] = True
+                st.session_state['username'] = username
+                st.experimental_rerun()
+            else:
+                st.error('Incorrect username or password')
     elif choice == 'Register':
-        with st.sidebar.form("Register"):
+        # Registration logic here
+        with st.form("Register"):
             name = st.text_input('Name')
             age = st.number_input('Age', min_value=18, max_value=100, step=1)
             email = st.text_input('Email')
@@ -49,7 +41,11 @@ def main():
             submitted = st.form_submit_button("Register")
             if submitted:
                 register(name, age, email, city, card_type, credit_limit, company, job_segment,password)
-                st.sidebar.success('Registered successfully. Please go to the Login tab to login.')
+                # st.success('Registered successfully. Please go to the Login tab to login.')
+
+                st.success('Registered successfully. Please log in.')
+                st.session_state['navigation'] = 'Login'  # Change navigation to login
+                st.experimental_rerun()
 
 if __name__ == "__main__":
     main()
